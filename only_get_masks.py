9517b5c53d93e24e843ad7fd464fc9bffc6a8504
Hanan Li <hananli@umich.edu>
2020-11-05 03:31:13 -0500

    So facenet sucks ass

---
+++ only_get_masks.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+from pathlib import Path
+from PIL import Image
+
+base_path = "C:\\Users\\david\\Documents\\masked-face-recognition"
+masked_path = os.path.join(base_path, "AFDB_masked_face_dataset")
+nonmasked_path = os.path.join(base_path, "AFDB_face_dataset")
+mixed_path = os.path.join(base_path, "team_pictures")
+# if not os.path.isdir(mixed_path):
+#     os.mkdir(mixed_path)
+
+# entries = os.listdir(masked_path)
+# for entry in entries:
+#     src = os.path.join(masked_path, entry)
+#     dst = os.path.join(mixed_path, entry)
+#     shutil.copytree(src, dst)
+#     non_masked_dir = os.path.join(nonmasked_path, entry)
+#     nonmasked_entries = os.listdir(non_masked_dir)
+#     for i in range(5):
+#         src_filename = os.path.join(non_masked_dir, nonmasked_entries[i])
+#         dst_filename = os.path.join(dst, nonmasked_entries[i])
+#         shutil.copyfile(src_filename, dst_filename)
+
+
+mixed_entries = os.listdir(mixed_path)
+for entry in mixed_entries:
+    src = os.path.join(mixed_path, entry)
+    for entry in os.listdir(src):
+        img_path = os.path.join(src, entry)
+        if entry == ".DS_Store":
+            os.remove(img_path)
+        else:
+            img = Image.open(img_path)
+            img = img.resize((160,160))
+            img.save(img_path)

29a4ac82552c6a02fcbe80b4a35431cff5901bb4
Hanan Li <hananli@umich.edu>
2020-11-04 22:25:43 -0500

    Change to Pytorch

---
+++ verify.py
@@ -0,0 +1,15 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1, fixed_image_standardization, training
+import torch
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from torch import optim
+from torch.optim.lr_scheduler import MultiStepLR
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
+import numpy as np
+import os
+
+def read_data(directory):
+    print(directory)
+if __name__ == "__main__":
+    directory = sys.argv[1]
+    read_data(directory)
